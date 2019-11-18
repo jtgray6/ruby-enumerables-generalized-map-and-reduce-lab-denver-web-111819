3011b1c1a358dd376a968b89a8c8bfd08e0f79d6
Jeremy Gray <jeremygray@utexas.edu>
2019-11-18 13:01:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
 # Your Code Here
+
+def map(array)
+ new=[]
+ i=0
+ while i<array.length do
+   new.push(yield(array[i]))
+   i+=1
+ end
+ new
+end
+
+def reduce(array, starting_point=0)
+  total=starting_point
+  i=0
+  while i<array.length
+    total+=array[i]
+    i+=1
+  end
+  j=0
+  while i<array.length
+    if !!array[j]
+      final= true
+    else
+      final= false
+    end
+    i+=1
+  end
+  final
+  total
+end
